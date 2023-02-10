62269a3184f8fe737f43912e0907cdde511a0aee
0502254 <124740486+0502254@users.noreply.github.com>
2023-02-10 12:43:19 +1100

    Start button

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,7 +15,11 @@ func _process(delta):
 		
 		
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 		move_and_collide(Vector2(-movement_speed * delta, 0))
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		move_and_collide(Vector2(movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_up"):
+		move_and_collide(Vector2(0, -movement_speed * delta))
+	if Input.is_action_pressed("ui_down"):
+		move_and_collide(Vector2(0, movement_speed * delta))
