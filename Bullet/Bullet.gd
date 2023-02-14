a7434c0c9a54c3ca91eec3bc5bff75cf859cbfc7
0502254 <124740486+0502254@users.noreply.github.com>
2023-02-15 09:51:08 +1100

    Bullets

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -4,7 +4,20 @@ var speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GlobalVariables.bulletInstanceCount += 1
 	set_physics_process(true)
 
 func _physics_process(delta):
 	var collidedOject = move_and_collide(Vector2(0, -speed*delta))
+
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		if GlobalVariables.bulletInstanceCount < 3:
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().getroot().add_child(bulletInstance)
+				if Input.is_action_just_pressed("fire")
+		if GlobalVariables.bulletInstanceCount < 3:
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().getroot().add_child(bulletInstance)	if Input.is_action_just_pressed("fire")
