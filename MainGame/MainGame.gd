9b5d71df3e49f7b8a280a733203ef9facfced165
0502254 <124740486+0502254@users.noreply.github.com>
2023-02-15 09:20:09 +1100

    Counter

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,15 +1,12 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export(int) var countDownMax
+var currentTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	currentTimer = countDownMax
+	$HUD/Countdown.text = str(currentTimer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
